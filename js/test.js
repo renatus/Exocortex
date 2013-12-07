6b0b56ab0d656737f3841ebd19d07c568b5101b9
renatus <pub@renat.biz>
2013-12-07 15:23:53 +0400

    Test time conversion in Firefox and Chrome

--- js/test.js
+++ js/test.js
@@ -102,9 +102,10 @@ var kelly = testdb({id:2}).first();
     //alert(curDayStart);
     //alert(plannedStartTimestamp);
     //alert(Date.parse("Thu, 01 Jan 1970 00:00:00 GMT-0400"));
-    alert("2013-12-07 00:00:00: " + Date.parse("2013-12-07 00:00:00"));
-    alert("2013-12-07T00:00:00: " + Date.parse("2013-12-07T00:00:00"));
-    alert("2013-12-07: " + Date.parse("2013-12-07"));
+    alert("2013-12-07 00:00:00: " + Date.parse("2013/12/07 00:00:00"));
+    //alert("2013-12-07 00:00:00: " + Date.parse("2013-12-07 00:00:00"));
+    //alert("2013-12-07T00:00:00: " + Date.parse("2013-12-07T00:00:00"));
+    //alert("2013-12-07: " + Date.parse("2013-12-07"));
     
     
     
