19b6dbdf73777361d40f80ad9e40a451ac184ba8
renatus <pub@renat.biz>
2013-12-09 22:42:16 +0400

    Test Moment JS lib

--- js/test.js
+++ js/test.js
@@ -118,7 +118,7 @@ var kelly = testdb({id:2}).first();
     //alert(moment());
     //alert(moment.parseZone("2013-01-01T00:00:00-13:00").zone());
     //alert(moment({hour: 23, minute: 59, seconds: 59}));
-    alert(moment().add('days', 1).startOf('day'));
+    alert(moment().add('days', 1).startOf('day').format());
     alert(moment().add('days', -1).endOf('day'));
     //alert(moment({hour: 0, minute: 0, seconds: 0}));
     //alert(moment().day(3));
