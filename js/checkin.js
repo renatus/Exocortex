b58edf2f4ac42371703977f6ae39fb5a07c834d2
renatus <pub@renat.biz>
2013-12-12 07:23:29 +0400

    Test checkin

--- js/checkin.js
+++ js/checkin.js
@@ -167,7 +167,8 @@ function checkin_sync_to_backend(entryID) {
                      '&node[field_latlon_accuracy][und][0][value]=' + curEntry.latLonAccuracy +
                      '&node[field_altitude][und][0][value]=' + curEntry.altitude +
                      '&node[field_altitude_accuracy][und][0][value]=' + curEntry.altitudeAccuracy +
-                     '&node[field_heading][und][0][value]=' + curEntry.heading +
+                     //'&node[field_heading][und][0][value]=' + curEntry.heading +
+                     '&node[field_heading][und][0][value]=' + '34.8956734734357733777' +
                      '&node[field_speed][und][0][value]=' + curEntry.speed +        
                      '&node[field_datetime_start][und][0][value][date]=' + curEntry.date +
                      '&node[field_datetime_start][und][0][value][time]=' + curEntry.time +
