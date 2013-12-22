f55b3b8336e594035a6d709106f25dd817574603
renatus <pub@renat.biz>
2013-12-22 07:39:09 +0400

    Reload Activity list on app DB update

--- js/pageshow_page_update.js
+++ js/pageshow_page_update.js
@@ -8,6 +8,6 @@
 //"pageshow" is deprecated since JQM 1.4, should be replaced by "pagecontainershow", but latter don't work for now
 $("div").on("pageshow", function(event, ui){
     //Reload list of Activities titles if we're on Activities list page for specified period of time (i.e. for today, or for future)
-    alert('s');
-    //reload_activities_list();
+    alert('z');
+    reload_activities_list();
 });
