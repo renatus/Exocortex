56ace7b711190ced1b4f4a106cf81c5483e160f3
renatus <pub@renat.biz>
2014-01-09 06:20:21 +0400

    Switch to IndexedDB

--- js/test.js
+++ js/test.js
@@ -149,11 +149,11 @@ var kelly = testdb({id:2}).first();
     
     
 	
-	alert((21.899999618530273).toPrecision(13));
+	//alert((21.899999618530273).toPrecision(13));
 	
-	var curDateTime = new Date();
-    var curTime = moment(curDateTime).format('HH:mm:ss');
-	alert(curTime);
+	//var curDateTime = new Date();
+    //var curTime = moment(curDateTime).format('HH:mm:ss');
+	//alert(curTime);
 	
 	
     
@@ -161,9 +161,111 @@ var kelly = testdb({id:2}).first();
     
     
 	
+	
+	
+	
+	
+	
+	
+	
+	
+      var dbName = "jqm-todo";
+      var dbVersion = 1;
+      var todoDB = {};
+      var indexedDB = window.indexedDB;
 
-    
-    
+      todoDB.indexedDB = {};
+      todoDB.indexedDB.db = null;
+	
+	  todoDB.indexedDB.open();
+	  todoDB.indexedDB.addTodo("Tst");
+
+      todoDB.indexedDB.onerror = function(e) {
+        console.log(e);
+      };
+
+      todoDB.indexedDB.open = function() {
+        var request = indexedDB.open(dbName, dbVersion);
+
+        request.onsuccess = function(e) {
+          console.log ("success our DB: " + dbName + " is open and ready for work");
+          todoDB.indexedDB.db = e.target.result;
+          todoDB.indexedDB.getAllTodoItems();
+        }
+        
+        request.onupgradeneeded = function(e) {
+          todoDB.indexedDB.db = e.target.result;
+          var db = todoDB.indexedDB.db;
+          console.log ("Going to upgrade our DB from version: "+ e.oldVersion + " to " + e.newVersion);
+
+            try {
+              if (db.objectStoreNames && db.objectStoreNames.contains("todo")) {
+                db.deleteObjectStore("todo");
+              }
+            }
+            catch (err) {
+              console.log("got err in objectStoreNames:" + err);
+            }
+            var store = db.createObjectStore("todo",
+                {keyPath: "timeStamp"});
+            console.log("-- onupgradeneeded store:"+ JSON.stringify(store));
+          }
+       
+        request.onfailure = function(e) {
+          console.error("could not open our DB! Err:"+e);  
+        }
+        
+        request.onerror = function(e) {
+          console.error("Well... How should I put it? We have some issues with our DB! Err:"+e);
+        }
+      };
+
+      todoDB.indexedDB.addTodo = function(todoText) {
+        var db = todoDB.indexedDB.db;
+        var trans = todoDB.indexedDB.db.transaction("todo", "readwrite");
+        var store = trans.objectStore("todo");
+
+        var data = {
+          "text": todoText,
+          "timeStamp": new Date().getTime()
+        };
+
+        var request = store.put(data);
+
+        request.onsuccess = function(e) {
+          todoDB.indexedDB.getAllTodoItems();
+        };
+
+        request.onerror = function(e) {
+          console.error("Error Adding an item: ", e);
+        };
+      };
+
+      todoDB.indexedDB.getAllTodoItems = function() {
+        var todos = document.getElementById("todoItems");
+        todos.innerHTML = "";
+
+        var db = todoDB.indexedDB.db;
+        var trans = db.transaction("todo", "readonly");
+        var store = trans.objectStore("todo");
+
+        // Get everything in the store;
+        var keyRange = IDBKeyRange.lowerBound(0);
+        var cursorRequest = store.openCursor(keyRange);
+
+        cursorRequest.onsuccess = function(e) {
+          var result = e.target.result;
+          if(!!result == false)
+            return;
+
+          alert(result.value);
+          result.continue();
+        };
+
+        cursorRequest.onerror = todoDB.indexedDB.onerror;
+      };
+
+
     
     
     
