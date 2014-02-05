a8a984412365295e9a9b41862eaadd9c710b6eac
renatus <pub@renat.biz>
2014-02-06 03:52:21 +0400

    Test setNotification

--- js/test.js
+++ js/test.js
@@ -586,3 +586,8 @@ function setDBentry(){
         };
     };
 }
+
+function notTest(){
+	//Display system-level notification
+	setNotification('Timebox is finished!', 'You can void it, accept it, or accept and immediately start a new one.');
+}
